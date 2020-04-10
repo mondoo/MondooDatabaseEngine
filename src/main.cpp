c5b85a80cb23af1e2d1c5e7ae2ae3d7fef9faa2a
Connor <mondocondo@hotmail.co.uk>
2020-04-10 02:57:17 +0100

    Added standard SQL statements

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
 #include "public/SQL.h"
 #include "public/DB.h"
-
+#include <stdio.h>
 #include <map>
 
 void main()
 {
 	DB* db = new DB("test.db");
 
-	db->Statement("CREATE TABLE test(ID INTEGER PRIMARY KEY AUTOINCREMENT, PATH INT NOT NULL, TYPE INT NOT NULL);");
-	db->Insert("test",	std::map<std::string, std::string>{
+	//db->Statement("CREATE TABLE test(ID INTEGER PRIMARY KEY AUTOINCREMENT, PATH INT NOT NULL, TYPE INT NOT NULL);");
+	int lastInsert = db->Insert("test",	std::map<std::string, std::string>{
 		{"PATH", "2"},
 		{"TYPE", "3"}
 	});
+
+	printf("New Row in Table: %i\n", lastInsert);
 }
--- src/public/DB.h
+++ src/public/DB.h
@@ -2,14 +2,9 @@
 
 #include <fmt/format.h>
 #include <string>
-#include "SQL.h"
 #include <map>
 
-#include <iostream>
-
-#include <stdio.h>
-
-#pragma optimize("", off)
+#include "SQL.h"
 
 class DB : public SQL
 {
@@ -25,64 +20,88 @@ public:
 		Statement(fmt::format("SELECT * FROM {} WHERE {} = {}", args...));
 	}
 
-	void Insert(const std::string& table, std::map<std::string, std::string>& insertMap)
+	int Insert(const std::string& table, std::map<std::string, std::string>& insertMap)
 	{
 		std::string key = "";
 		std::string value = "";
+		GenerateKeyValue(insertMap, key, value);
+
+		auto callback = [](void* instance, int argc, char** argv, char** colName) {
+			return static_cast<DB*>(instance)->InsertCallback(instance, argc, argv, colName);
+		};
+
+		Statement(fmt::format("INSERT INTO {} ({}) VALUES ({}); SELECT last_insert_rowid();", table, key, value), callback, this);
+
+		int insertIDOut = m_insertIDOut;
+		m_insertIDOut = -1;
+		return insertIDOut;
+	}
+
+	template<typename... Args>
+	void Update(Args... args)
+	{
+		auto callback = [](void* instance, int argc, char** argv, char** colName) {
+			return static_cast<DB*>(instance)->UpdateCallback(instance, argc, argv, colName);
+		};
+
+		Statement(fmt::format("UPDATE {} SET {} = {} WHERE {}={}", args...), callback, this);
+	}
+
+	template<typename... Args>
+	void Delete(Args... args)
+	{
+		auto callback = [](void* instance, int argc, char** argv, char** colName) {
+			return static_cast<DB*>(instance)->DeleteCallback(instance, argc, argv, colName);
+		};
+
+		Statement(fmt::format("DELETE FROM {} WHERE {}={}", args...), callback, this);
+	}
+
+	void Statement(const std::string& statement, int (*callback)(void*, int, char**, char**) = NULL, void* self = nullptr)
+	{
+		Exec(statement, callback, self);
+	}
+
+private:
+	int InsertCallback(void* instance, int argc, char** argv, char** columnName)
+	{
+		if (argc >= 0)
+		{
+			m_insertIDOut = atoi(argv[0]);
+			return 0;
+		}
+		return 1;
+	}
+
+	int UpdateCallback(void* instance, int argc, char** argv, char** columnName)
+	{
+		return 0;
+	}
+
+	int DeleteCallback(void* instance, int argc, char** argv, char** columnName)
+	{
+		return 0;
+	}
+
+	void GenerateKeyValue(std::map<std::string, std::string>& insertMap, std::string& outKey, std::string& outValue)
+	{
 		bool first = true;
 		for (std::map<std::string, std::string>::iterator it = insertMap.begin(); it != insertMap.end(); ++it)
 		{
 			if (first)
 			{
-				key += it->first;
-				value += it->second;
+				outKey += it->first;
+				outValue += it->second;
 				first = false;
 			}
 			else
 			{
-				key += ", " + it->first;
-				value += ", " + it->second;
+				outKey += ", " + it->first;
+				outValue += ", " + it->second;
 			}
 		}
-
-		auto CB = [](void* instance, int argc, char** argv, char** colName) {
-			std::cout << "Hello world!" << std::endl;
-			if (argc >= 0)
-			{
-				return static_cast<DB*>(instance)->PrintUpdate(argc, argv, colName);
-			}
-			return -1;
-		};
-
-		Statement(fmt::format("INSERT INTO {} ({}) VALUES ({}); SELECT last_insert_rowid();", table, key, value), CB, this);
-
-		Statement("SELECT * FROM test", CB, this);
-		
 	}
 
-	int PrintUpdate(int outCount, char** outArgv, char** outColName)
-	{
-		int i = 0;
-		for (i = 0; i < outCount; i++)
-		{
-			printf("%s = %s\n", outColName[i], outArgv[i] ? outArgv[i] : "NULL");
-		}
-
-		return 0;
-	}
-
-	void Update()
-	{
-
-	}
-
-	void Delete()
-	{
-
-	}
-
-	void Statement(const std::string& statement, int (*fn)(void*, int, char**, char**) = NULL, void* classPtr = nullptr)
-	{
-		Exec(statement, fn, classPtr);
-	}
+private:
+	int m_insertIDOut = -1;
 };
