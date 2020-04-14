dce170489c196d28316f30d05f1e15d74bdf2e8f
Connor <mondocondo@hotmail.co.uk>
2020-04-14 23:56:09 +0100

    Started implementation of Model class

--- src/public/Model.h
+++ src/public/Model.h
@@ -1,14 +1,31 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
+#include "DB.h"
+#include "Table.h"
+#include "Types.h"
+
+template<class TModel>
 class Model
 {
 public:
-	Model(const std::string& table)
-		: m_table(table)
-	{}
+	Model() {}
+
+	static std::vector<TModel*> All()
+	{
+		std::vector<TModel*> models;
+		DB::Table(m_table)->Select()->ExecStatement(true, TModel::Callback, &models);
+		return models;
+	}
 
 protected:
-	std::string m_table;
+	inline static std::string m_table = "test";
+	inline static std::string m_primaryKey = "ID";
+	inline static bool m_isAutoIncrementing = true;
+	inline static bool m_hasTimestamps = true;
+
+	inline static std::string m_createdAt = "CREATED_AT";
+	inline static std::string m_updatedAt = "UPDATED_AT";
 };
